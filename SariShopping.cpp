46ea4b6f757ea12b748b711de3de545a6bfbca07
ashuvishwkarma <bscmathspirate37659@gmail.com>
2021-05-19 09:07:53 +0530

    updated

--- SariShopping.cpp
+++ SariShopping.cpp
@@ -1,3 +1,27 @@
+/* 
+####   Documentization
+
+This content is released under the MIT License (MIT)
+
+Copyright (c) 2021 , Sanheen Sethi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    @project SariShopping
+    @author Sanheen Sethi
+    @copyright Copyright (c) 2021, Sanheen Sethi
+    @license http://opensource.org/licenses/MIT	MIT License
+    @link https://github.com/sanheensethi
+    @version version 1.0.0
+    @filesource
+    @Date : 19-05-2021
+    @Time : 10:10 PM
+
+*/
 #include<iostream>
 #include<string>
 #include<fstream>
